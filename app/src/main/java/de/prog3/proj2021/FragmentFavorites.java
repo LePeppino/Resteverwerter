2800d426f56292e55bcc6f10ca5a21401fc6b8bf
EricWalter5174 <eric.walter@tesos.de>
2021-01-26 17:08:55 +0100

    Bottom Nav Bar implemented

---
+++ app/src/main/java/de/prog3/proj2021/FragmentFavorites.java
@@ -0,0 +1,19 @@
+package de.prog3.proj2021;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+
+public class FragmentFavorites extends Fragment {
+
+    @Nullable
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.fragment_favorites, container, false);
+    }
+}
--- app/src/main/java/de/prog3/proj2021/MainActivity.java
+++ app/src/main/java/de/prog3/proj2021/MainActivity.java
@@ -12,12 +12,17 @@ package de.prog3.proj2021;
 *
 * */
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 import androidx.room.Room;
 
 import android.os.Bundle;
+import android.view.MenuItem;
+
+import com.google.android.material.bottomnavigation.BottomNavigationView;
 
 import java.util.ArrayList;
 
@@ -45,6 +50,10 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        BottomNavigationView bottomNavigationView = findViewById(R.id.bottom_nav);
+        bottomNavigationView.setOnNavigationItemSelectedListener(navListener);
+
+
         //create an instance of the database and preload with data
         //TODO: create preloaded database asset or file to insert data
         AppDatabase db = Room.databaseBuilder(getApplicationContext(), AppDatabase.class, "Sample.db")
@@ -60,14 +69,40 @@ public class MainActivity extends AppCompatActivity {
         ShoppingListDao shoppingListDao = db.shoppingListDao();
 
         //RecipeRecyclerViewAdapter
-        initRecipeList();
+        //initRecipeList();
 
     }
+
+    private BottomNavigationView.OnNavigationItemSelectedListener navListener =
+            new BottomNavigationView.OnNavigationItemSelectedListener() {
+                @Override
+                public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+                    Fragment selectedFragment = null;
+
+                    switch(item.getItemId()){
+                        case R.id.nav_home:
+                            selectedFragment = new FragmentHome();
+                            break;
+                        case R.id.nav_picker:
+                            selectedFragment = new FragmentPicker();
+                            break;
+                        case R.id.nav_shopping_list:
+                            selectedFragment = new FragmentShoppingList();
+                            break;
+                        case R.id.nav_favourites:
+                            selectedFragment = new FragmentFavorites();
+                    }
+                    getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container,
+                            selectedFragment).commit();
+
+                    return true;
+                }
+            };
 
     /*
     * Queries recipes from database and passes data to RecipeRecyclerViewAdapter
     * */
-    private void initRecipeList(){
+    /*private void initRecipeList(){
         //TODO: Query database
 
         //example data
@@ -80,15 +115,15 @@ public class MainActivity extends AppCompatActivity {
         mRecipeDescriptions.add("lorem ipsum 2");
 
         initRecyclerView();
-    }
+    }*/
 
     /*
     * initialise RecipeRecyclerView with Adapter
     * */
-    private void initRecyclerView(){
+   /* private void initRecyclerView(){
         RecyclerView recipeRecyclerView = findViewById(R.id.recipeRecyclerView);
         RecipeRecyclerViewAdapter adapter = new RecipeRecyclerViewAdapter(this, mRecipeNames, mRecipeDescriptions, mImageUrls);
         recipeRecyclerView.setAdapter(adapter);
         recipeRecyclerView.setLayoutManager(new LinearLayoutManager(this));
-    }
+    }*/
 }
